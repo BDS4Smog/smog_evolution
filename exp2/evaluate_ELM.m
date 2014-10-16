fc22af4b14d58f824c23cba84bb843720a2bf424
Jiaoyan Chen <chen00217@gmail.com>
2014-10-16 22:53:39 +0800

    change

--- exp2/evaluate_ELM.m
+++ exp2/evaluate_ELM.m
@@ -12,18 +12,18 @@ if type==1
 end
 
 if type==2
-    range = 2:14;
+    range = 12;
 end
 
 ROUND_NUM = 4;
 
-d0 = load(['data/' station '_high.txt']);
+d0 = load(['data/' station '_low.txt']);
 d0 = [zeros(size(d0,1),1) d0(:,range)];
 d0 = d0(randperm(length(d0)),:); 
-d0 = d0(1:198,:);
-d1 = load(['data/' station '_decrease.txt']);
+d0 = d0(1:180,:);
+d1 = load(['data/' station '_increase.txt']);
 d1 = [ones(size(d1,1),1) d1(:,range)];
-d1 = d1(1:198,:);
+d1 = d1(1:180,:);
 d = [d0' d1']';
 d = d(randperm(length(d)),:); 
 Train_Accuracy = 0;
@@ -39,7 +39,7 @@ for i = 1:ROUND_NUM
     else
         Tr = [d(1:start_1-1,:)',d(end_1+1:length(d),:)']';    
     end
-    [Tr_acc, Te_acc] = my_ELM(Tr, Te, 1, 30, 'sig');
+    [Tr_acc, Te_acc] = my_ELM(Tr, Te, 1, 40, 'sig');
     Train_Accuracy = Train_Accuracy + Tr_acc;
     Test_Accuracy = Test_Accuracy + Te_acc;
 end
