a48cea0832a8673f76ef7e4593a1de040a8cfbae
vacant3085 <vest3085@163.com>
2014-10-29 17:10:56 +0800

    All_recollected

--- exp2/evaluate_ELM.m
+++ exp2/evaluate_ELM.m
@@ -2,27 +2,76 @@ function evaluate_ELM( range,station )
 %EVALUATE_ELM Summary of this function goes here
 %   Detailed explanation goes here
 %Local air
-station = 'gucheng';
-range = [2 11:14];
-
-f1 = ['data/' station '_increase.txt'];
-f0 = ['data/' station '_low.txt'];
+station = 'haidian';
+type1 = 'increase'
+type2 = 'low'
 
 HIDDEN_NUM = 50;
 ROUND_NUM = 4;
-REPEAT_NUM = 10;
+REPEAT_NUM =200;
+LIMIT_OF_EMPTY = 15
+
+field = [1 1 1 1]
+
+air_range = [2:7];
+mete_range = [2:8];
+air_surround_range = [2:6];
+mete_surround_range = [2:21];
+
+air_f1 = ['air/' station '_' type1 '.txt'];
+air_f0 = ['air/' station '_' type2 '.txt'];
+mete_f1 = ['mete/' station '_' type1 '.txt'];
+mete_f0 = ['mete/' station '_' type2 '.txt'];
+air_surround_f1 = ['air_surround/' station '_' type1 '.txt'];
+air_surround_f0 = ['air_surround/' station '_' type2 '.txt'];
+mete_surround_f1 = ['mete_surround/' station '_' type1 '.txt'];
+mete_surround_f0 = ['mete_surround/' station '_' type2 '.txt'];
+
+
+d1 = []
+d0 = []
+if field(1)==1
+    tmp_d = load(air_f1);
+    d1 = [d1 tmp_d(:,air_range)]
+    tmp_d = load(air_f0);
+    d0 = [d0 tmp_d(:,air_range)]
+end
+if field(2)==1
+    tmp_d = load(mete_f1);
+    d1 = [d1 tmp_d(:,mete_range)]
+    tmp_d = load(mete_f0);
+    d0 = [d0 tmp_d(:,mete_range)]
+end
+if field(3)==1
+    tmp_d = load(air_surround_f1);
+    d1 = [d1 tmp_d(:,air_surround_range)]
+    tmp_d = load(air_surround_f0);
+    d0 = [d0 tmp_d(:,air_surround_range)]
+end
+if field(4)==1
+    tmp_d = load(mete_surround_f1);
+    d1 = [d1 tmp_d(:,mete_surround_range)]
+    tmp_d = load(mete_surround_f0);
+    d0 = [d0 tmp_d(:,mete_surround_range)]
+end
+
+%    tmp_d1 = [max((d1(:,[15 21 27 33]))')' min((d1(:,[15 21 27 33]))')'];
+d1 = [ones(size(d1,1),1) d1];
+
+%    d1 = compRecord(d1);
+d1 = extract_record(d1,LIMIT_OF_EMPTY);
+%    tmp_d0 = [max((d0(:,[15 21 27 33]))')' min((d0(:,[15 21 27 33]))')'];
+d0 = [zeros(size(d0,1),1) d0];
+%    d0 = compRecord(d0);
+d0 = extract_record(d0,LIMIT_OF_EMPTY);
+   
 
 Train_Accuracy = 0;
-Test_Accuracy = 0;
+Test_Accuracy = 0; 
+
+n_record = size(d1,1);
 for k = 1:REPEAT_NUM
-    d1 = load(f1);
-%    tmp_d1 = [max((d1(:,[15 21 27 33]))')' min((d1(:,[15 21 27 33]))')'];
-    d1 = [ones(size(d1,1),1) d1(:,range)];
-%    d1 = compRecord(d1);
-    d0 = load(f0);
-%    tmp_d0 = [max((d0(:,[15 21 27 33]))')' min((d0(:,[15 21 27 33]))')'];
-    d0 = [zeros(size(d0,1),1) d0(:,range)];
-%    d0 = compRecord(d0);
+
     d0 = d0(randperm(length(d0)),:); 
     d0 = d0(1:size(d1,1),:);
     d = [d0' d1']';
@@ -49,14 +98,26 @@ Train_Accuracy = Train_Accuracy/(ROUND_NUM*REPEAT_NUM);
 Test_Accuracy = Test_Accuracy/(ROUND_NUM*REPEAT_NUM);
 fprintf('Train_Accuracy: %f \n',Train_Accuracy);
 fprintf('Test_Accuracy: %f \n',Test_Accuracy);
+fprintf('Number of records used is %d\n',n_record*2);
 
 end
 
 function [r_d] = compRecord(d)
+    
     for col = 2:size(d,2)
         d = d(find(d(:,col)~=-1),:);
     end
     r_d = d;
+end
+
+function [rd] = extract_record(d,limit)
+    rd = [];
+    [M N]=size(d);
+    for row = 1:M
+       if(size(find(d(row,:)==-1),2)<=limit)
+           rd = [rd;d(row,:)];
+       end
+    end
 end
 
 function [n_d] = myNormalize(d)
