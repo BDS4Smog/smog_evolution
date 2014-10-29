7c3e8b4b2df1c8aebf69e33f88f90d54ce6257d2
Jiaoyan Chen <chen00217@gmail.com>
2014-10-29 16:55:50 +0800

    traffic data

--- exp2/genData_traffic.py
+++ exp2/genData_traffic.py
@@ -3,19 +3,21 @@ import pymongo
 import os
 import sys
 import time
+import datetime
 
-DB_NAME = 'CityWeibo_API'
+DB_NAME = 'tmp_weibo'
 C_NAME = 'beijing'
 HOST = '10.214.0.147'
 PORT = 27017
 ISOTIMEFORMAT='%Y-%m-%dT%XZ'
 STEP = 6
 DELT_T = 12
-EDGE = 0.2
+EDGE = 0.25
 C_LAT = 39.87275
 C_LON = 116.3057
 
-WORDS = [u'堵车',u'塞车']
+POSITIVE_WORDS = [u'堵车',u'塞车',u'堵死了',u'车好多',u'道路拥堵',u'行驶缓慢']
+NEGATIVE_WORDS = [u'私信订阅路况，微博应对堵车']
 
 def getTime(file_name):
     f = open(file_name)
@@ -36,128 +38,74 @@ def expendTime(T):
             eT.append(tmp_time)    
     return eT
 
-def saveFile(lines,file_name)
-    f = open(file_name)
+def saveFile(lines,file_name):
+    f = open(file_name,'w')
     f.writelines(lines)
     f.close()
 
 def tweet_filter(t,c):
     traffic_num = 0
     tweet_num = 0
-    q_s = '.*' + num2month(t[5:7]) + ' ' + t[8:10] + '.*' + t[0:4]
-    records = c.find({'created_at':{'$regex':q_s}})
+    start_dt,end_dt = getTimeRange(t)
+    records = c.find({'created_at':{'$lt':end_dt,'$gt':start_dt}})
     for r in records:
         text = r['text']
-        created_at = r['created_at']
-        lat = r['geo']['coordinates'][0]
-        lon = r['geo']['coordinates'][1]
-        if inTime(created_at,t) and inArea(lat,lon):
+        if inArea(r['lat'],r['lon']):
             tweet_num += 1
             if hasKeyword(text):
+                print text
                 traffic_num += 1
     return traffic_num,tweet_num
 
-def getQueryStr(t):
-    unix_current = time.mktime(time.strptime(t,ISOTIMEFORMAT))    
-    unix_tmp = unix_current-3600
-    t = time.strftime(ISOTIMEFORMAT,time.localtime(unix_tmp))
-    q_s = '(.*' + num2month(t[5:7]) + ' ' + t[8:10] + ' ' + t[11:13] +  '.*' + t[0:4] + ')'
-    for i in range(1,DELT_T+1):
-        unix_tmp = unix_current-i*3600
-        t = time.strftime(ISOTIMEFORMAT,time.localtime(unix_tmp))
-        q_s = q_s + '|' + '(.*' + num2month(t[5:7]) + ' ' + t[8:10] + ' ' + t[11:13] +  '.*' + t[0:4] + ')'
-    return "{'created_at':{'$regex':" + q_s + "}}"
-
-    
-def num2month(s):
-    if s=='01':
-        return 'Jan'
-    if s=='02':
-        return 'Feb'
-    if s=='03':
-        return 'Mar'
-    if s=='04':
-        return 'Apr'
-    if s=='05':
-        return 'May'
-    if s=='06':
-        return 'Jun'
-    if s=='07':
-        return 'Jul'
-    if s=='08':
-        return 'Aug'
-    if s=='09':
-        return 'Sep'
-    if s=='10':
-        return 'Oct'
-    if s=='11':
-        return 'Nov'
-    if s=='12':
-        return 'Dec'
-
-def month2num(s):
-    if s=='Jan':
-        return '01'
-    if s=='Feb':
-        return '02'
-    if s=='Mar':
-        return '03'
-    if s=='Apr':
-        return '04'
-    if s=='May':
-        return '05'
-    if s=='Jun':
-        return '06'
-    if s=='Jul':
-        return '07'
-    if s=='Aug':
-        return '08'
-    if s=='Sep':
-        return '09'
-    if s=='Oct':
-        return '10'
-    if s=='Nov':
-        return '11'
-    if s=='Dec':
-        return '12'
-
+def getTimeRange(t):
+    unix_current = time.mktime(time.strptime(t,ISOTIMEFORMAT)) 
+    end_dt = datetime.datetime.fromtimestamp(unix_current)
+    unix_tmp = unix_current - DELT_T*3600
+    start_dt = datetime.datetime.fromtimestamp(unix_tmp)
+    return start_dt,end_dt
 
 def inArea(lat, lon):
-    if lat > C_LAT + EDGE or lat < C_ALT - EDGE:
+    if lat > C_LAT + EDGE or lat < C_LAT - EDGE:
         return False
     if lon > C_LON + EDGE or lon < C_LON - EDGE:
         return False
     return True
        
 def hasKeyword(text):
-    for word in WORDS:
+    for word in NEGATIVE_WORDS:
+        if word in text:
+            return False
+    for word in POSITIVE_WORDS:
         if word in text:
             return True
     return False
 
-    
-
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print 'usage: python genData_traffic.py decrease/increase high/low result1 result0'
+    if len(sys.argv) != 2:
+        print 'usage: python genData_traffic.py station'
         sys.exit(0)
-    T1 = getTime(sys.argv[1])
+    station = sys.argv[1]
+    T1 = getTime('events/'+station+'_increase.txt')
     T1 = expendTime(T1)
-    T0 = getTime(sys.argv[2])
+    T0 = getTime('events/'+station+'_low.txt')
     result1 = []
     result0 = []
     conn = pymongo.Connection(HOST,PORT)
     db = conn[DB_NAME]
     c = db[C_NAME]
+    print 'for increase time stamps'
     for t in T1:
+        print t
         traffic_num,tweet_num = tweet_filter(t,c)
         pec = '%.2f' % (float(traffic_num)/float(tweet_num))
-        result1.append(t + ' ' + pec + ' ' + traffic_num + ' ' + tweet_num + '\r\n')
+        result1.append(t+' '+pec+' '+str(traffic_num)+' '+str(tweet_num)+'\r\n')
+    saveFile(result1, 'data_traffic/'+station+'_increase_traffic.txt')
+    print 'for low time stamps'
     for t in T0:
+        print t
         traffic_num,tweet_num = tweet_filter(t,c)
         pec = '%.2f' % (float(traffic_num)/float(tweet_num))
-        result0.append(t + ' ' + pec + ' ' + traffic_num + ' ' + tweet_num + '\r\n')
+        result0.append(t+' '+pec+' '+str(traffic_num)+' '+str(tweet_num)+'\r\n')
+    saveFile(result0, 'data_traffic/'+station+'_low_traffic.txt')
     conn.close()
-    saveFile(result1, sys.argv[3])
-    saveFile(result0, sys.argv[4])
 
